cd5051c27f34d1c0f3e2ec719e3e26e155fb1016
Michael Ibarra <bm2yogi@gmail.com>
2013-04-18 17:28:40 -0700

    customized admin section.

---
+++ mysite/polls/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from polls.models import Poll, Choice
+
+class ChoiceInline(admin.TabularInline):
+	model = Choice
+	extra = 1
+
+class PollAdmin(admin.ModelAdmin):
+	fieldsets=[
+		('Questions', {'fields': ['question']}),
+		('Date Information', {'fields':['pub_date']}),
+	]
+	inlines=[ChoiceInline]
+	list_display = ('question','pub_date','was_published_recently')
+	list_filter = ['pub_date']
+	search_fields = ['question']
+	date_hierarchy = 'pub_date'
+
+admin.site.register(Poll, PollAdmin)
